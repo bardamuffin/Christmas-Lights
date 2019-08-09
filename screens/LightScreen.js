3f5364b54f8397bdfacea20ac7e63505db108b01
commodus <bardamuffin@gmail.com>
2019-08-09 10:39:08 +0200

    finished

--- screens/LightScreen.js
+++ screens/LightScreen.js
@@ -20,14 +20,3 @@ export default class LightScreen extends React.Component {
 const styles = StyleSheet.create({
     container: { flex: 1, flexDirection: "row", paddingTop: 50, padding: 20, justifyContent: "space-between", backgroundColor: "black"},
 });
-
-
-/*
-<Bulb style={{backgroundColor: "blue", width: 90, height: 90}}/>
-                <Bulb style={{backgroundColor: "red"}}/>
-                <Bulb style={{backgroundColor: "green"}}/>
-                <Bulb style={{backgroundColor: "yellow"}}/>
-                <Bulb style={{backgroundColor: "aqua"}}/>
-                <Bulb style={{backgroundColor: "gainsboro"}}/>
-                <Bulb style={{backgroundColor: "fuchsia"}}/>
-*/                
