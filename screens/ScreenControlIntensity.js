dacad9bb9092e61b433171b2f6e7a12fe20c45b9
commodus <bardamuffin@gmail.com>
2019-08-11 10:20:26 +0200

    sliders update state value

--- screens/ScreenControlIntensity.js
+++ screens/ScreenControlIntensity.js
@@ -1,20 +1,73 @@
 import React from 'react';
-import { StyleSheet, View, } from 'react-native';
+import { StyleSheet, View, Slider, Text } from 'react-native';
 import { Input } from 'react-native-elements';
 
 //Component to manage the range of bulb's opacity
 export default class ControlIntensity extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pickerValue: 0.5
+        }
+    }
+
+    handleChangeValue = val => {
+        console.log("Slider: ", typeof val)
+        this.setState({ pickerValue: this.val})
+    }
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.inputContainer}>
-                    
+                    <Text style={styles.label}>Opacity</Text>
+                    <Text style={styles.valueStart}>0</Text>
+                    <Slider
+                        style={{width: 200, height: 40, margin: 10}}
+                        minimumValue={0}
+                        maximumValue={1}
+                        value={this.state.pickerValue}
+                        step={0.1}
+                        onValueChange={e => this.handleChangeValue(e)}
+                    />
                 </View>
+                
+            <View style={styles.inputContainer}>
+            <Text style={styles.label}>Opacity</Text>
+            <Text style={styles.valueStart}>0</Text>
+            <Slider
+                style={{width: 200, height: 40, margin: 10, paddingLeft: 40}}
+                minimumValue={0}
+                maximumValue={1}
+                value={1}
+
+            />
+        </View>
             </View>
         )
     } 
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: "red", }
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black",
+    },
+    label: {
+        color: "#33691E",
+    },
+    valueStart: {
+        color: "#33691E",
+    },
+    inputContainer: { 
+        width: "50%",
+        height: "50%",
+        backgroundColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#33691E",
+        borderColor: "#33691E",
+        //borderWidth: 6,    
+    },
 })
