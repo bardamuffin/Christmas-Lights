0c1cc219ec23b78f94206dd0bc842595079eac13
commodus <bardamuffin@gmail.com>
2019-08-11 09:30:42 +0200

    added view container

--- screens/ScreenControlIntensity.js
+++ screens/ScreenControlIntensity.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { StyleSheet, View, } from 'react-native';
 import { Input } from 'react-native-elements';
 
+//Component to manage the range of bulb's opacity
 export default class ControlIntensity extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-
+                <View style={styles.inputContainer}>
+                    
+                </View>
             </View>
         )
     } 
